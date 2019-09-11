566b321892f3e2f23df493c4cded999ed9c0a85b
nomisum <nomisum@googlemail.com>
2019-09-11 20:39:11 +0200

    fix prep time

--- dialogs/preparationTime/rscTitles.hpp
+++ dialogs/preparationTime/rscTitles.hpp
@@ -1,12 +1,14 @@
+#include "..\ui_toolkit.hpp"
+
 #define CT_STRUCTURED_TEXT          13
 #define ST_LEFT                     0x00
 
-#define MITM_PREPARATIONTIME_W      pixelGridNoUIScale * pixelW * 10
-#define MITM_PREPARATIONTIME_H      pixelGridNoUIScale * pixelH * 3
-#define MITM_PREPARATIONTIME_X      safeZoneX + safeZoneW - MITM_PREPARATIONTIME_W
-#define MITM_PREPARATIONTIME_Y      safeZoneY + pixelGridNoUIScale * pixelH * 30
+#define GRAD_PREPARATIONTIME_W      (X_SCALE * 0.20)
+#define GRAD_PREPARATIONTIME_H      (Y_SCALE * 0.045)
+#define GRAD_PREPARATIONTIME_X      (safeZoneX + safeZoneW - GRAD_PREPARATIONTIME_W)
+#define GRAD_PREPARATIONTIME_Y      (safeZoneY + Y_SCALE * 0.5)
 
-class mitm_preparationTime {
+class grad_preparationTime {
     idd = -1;
     fadein = 0;
     fadeout = 1;
@@ -17,8 +19,8 @@ class mitm_preparationTime {
 
     class ControlsBackground {
         class Countdown {
-            onLoad = "uiNamespace setVariable ['mitm_preparationTime', _this select 0];";
-            onUnload = "uiNamespace setVariable ['mitm_preparationTime', controlNull];";
+            onLoad = "uiNamespace setVariable ['grad_preparationTime', _this select 0];";
+            onUnload = "uiNamespace setVariable ['grad_preparationTime', controlNull];";
 
             idc = -1;
             access = 0;
@@ -26,8 +28,9 @@ class mitm_preparationTime {
             style = ST_LEFT;
             colorBackground[] = {0,0,0,0.85};
 
-            size = 0.018;
-            text = "GAME STARTS IN: <br/>ASD";
+            size = 0.04 * TEXT_SCALE;
+            text = "";
+
             class Attributes {
                 font = "PuristaLight";
                 color = "#ffffff";
@@ -35,13 +38,13 @@ class mitm_preparationTime {
                 valign = "middle";
                 shadow = false;
                 shadowColor = "#ff0000";
-                size = "2.4";
+                size = 1;
             };
 
-            w = MITM_PREPARATIONTIME_W;
-            h = MITM_PREPARATIONTIME_H;
-            x = MITM_PREPARATIONTIME_X;
-            y = MITM_PREPARATIONTIME_Y;
+            w = GRAD_PREPARATIONTIME_W;
+            h = GRAD_PREPARATIONTIME_H;
+            x = GRAD_PREPARATIONTIME_X;
+            y = GRAD_PREPARATIONTIME_Y;
         };
     };
 };
